5e615cafbbeda21c900c5b7c3ea725222ad0d157
feven7060-eng <feven7060@gmail.com>
2026-03-18 11:39:56 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,13 +12,13 @@ export default defineConfig({
         https: false, // Use HTTP for frontend locally to avoid certificate handshake issues
         proxy: {
             '/api': {
-                target: 'https://localhost:5000', // Proxy still uses HTTPS to the backend
+                target: 'https://web-12h1.onrender.com', // Proxy still uses HTTPS to the backend
                 changeOrigin: true,
                 secure: false, 
                 rewrite: (path) => path 
             },
             '/uploads': {
-                target: 'https://localhost:5000',
+                target: 'https://web-12h1.onrender.com',
                 changeOrigin: true,
                 secure: false,
                 rewrite: (path) => path
