f2202310bd445b6cbdf2fdfb3661bbd99d9661b4
feven7060-eng <feven7060@gmail.com>
2026-03-18 15:28:20 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -5,6 +5,9 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL && process.env.DATABASE_URL.includes('localhost') ? false : {
+    rejectUnauthorized: false
+  }
 });
 
 export const query = (text, params) => pool.query(text, params);
