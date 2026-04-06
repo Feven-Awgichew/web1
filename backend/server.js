8511b928c6d5d2dcf881f639598e9a30001bcedc
feven7060-eng <feven7060@gmail.com>
2026-04-06 13:58:06 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -66,8 +66,11 @@ const upload = multer({ storage: storage });
 initDb();
 
 // Email Configuration
+// Email Configuration
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // Use STARTTLS
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
